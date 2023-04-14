b9eecef959c1e4ee67edc2bd851580261da3ceff
Blaser993 <corradodepinto.design@gmail.com>
2023-04-14 15:39:48 +0200

    FEAT added database, model e controller

---
+++ app/Models/Comic.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+
+use App\Http\Models\Comic;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comic extends Model
+{
+    use HasFactory;
+}
