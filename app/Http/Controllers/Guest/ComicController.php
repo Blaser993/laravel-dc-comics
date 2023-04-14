641d12efb145058927028e767e33eeb159d1f4f9
Blaser993 <corradodepinto.design@gmail.com>
2023-04-14 15:39:54 +0200

    ok

---
+++ app/Http/Controllers/Guest/ComicController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Guest\ComicController;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ComicController extends Controller
+{
+    //
+}
